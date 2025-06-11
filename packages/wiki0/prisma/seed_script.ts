9c55b752453094b08d6bc0642f35ec9e4e87b737
Sofia Desenberg <sofiadray+oktagithub@gmail.com>
2025-06-11 12:37:27 -0400

    Cherry pick from vatsal2727:VP_Add_support_for_multi_org branch

--- packages/wiki0/prisma/seed_script.ts
+++ packages/wiki0/prisma/seed_script.ts
@@ -12,6 +12,22 @@ export async function insertUsers() {
     },
   });
 
+  const org2 = await prisma.organization.create({
+    data: {
+      domain: 'tables.fake2',
+      name: 'Fake Org 2',
+      auth_server_key: 'customer2',
+    },
+  });
+
+  const org3 = await prisma.organization.create({
+    data: {
+      domain: 'tables.fake3',
+      name: 'Fake Org 3',
+      auth_server_key: 'customer3',
+    },
+  });
+
   const bobbyUser = await prisma.user.create({
     data: {
       name: 'Bobby Tables',
@@ -30,4 +46,24 @@ export async function insertUsers() {
     },
   });
   console.log('Created user Trinity', trinityUser);
+
+  const johnUser = await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'john@tables.fake2',
+      orgId: org2.id,
+      externalId: 'john@tables.fake2',
+    },
+  });
+  console.log('Created user John', johnUser);
+
+  const fooUser = await prisma.user.create({
+    data: {
+      name: 'Foo Bar',
+      email: 'foo@tables.fake3',
+      orgId: org3.id,
+      externalId: 'foo@tables.fake3',
+    },
+  });
+  console.log('Created user Foo', fooUser);
 }
