769865d31bfb8084eb0a067a7988849f6c717157
Sohail Pathan <iamspathan@gmail.com>
2025-06-24 13:35:33 +0000

    es module fix

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 // bootstrap.js: Cross-platform replacement for bootstrap.sh
-const { execSync } = require('child_process');
+import { execSync } from 'child_process';
 
 function run(cmd) {
   execSync(cmd, { stdio: 'inherit' });
--- scripts/open-apps.js
+++ scripts/open-apps.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 // open-apps.js: Cross-platform replacement for open-apps.sh
-const { exec } = require('child_process');
+import { exec } from 'child_process';
 
 function openUrl(url) {
   let start;
@@ -11,8 +11,15 @@ function openUrl(url) {
   } else {
     start = 'xdg-open';
   }
-  exec(`${start} ${url}`);
+  exec(`${start} "${url}"`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`[open-apps] Failed to open ${url}:`, error.message);
+    }
+  });
 }
 
+console.log(
+  '[open-apps] Opening http://localhost:3000/ and http://localhost:3001/ in your default browser...'
+);
 openUrl('http://localhost:3000/');
 openUrl('http://localhost:3001/');
